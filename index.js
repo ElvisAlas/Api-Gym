07da55376511c8b9f95622daea343c4083167555
Elvis Alas <110005376+ElvisAlas@users.noreply.github.com>
2024-05-12 21:48:13 -0600

    adicionando Middleware al index control de errores y rutas, Middleware para permitir el acceso a través de CORS

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
 import express from 'express';
 import { Evento } from './Routers/routeEvento.js';
-const app = express();
-const port = 7000;
 
-app.use(express.json())
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// Middleware para el análisis de JSON
+app.use(express.json());
+
+// Middleware para permitir el acceso a través de CORS
+
+app.use('*',(req, res, next) => {
+	const allowedOrigins = ['http://localhost:3000', 'http://localhost:5173'];
+	const origin = req.headers.origin;
+	if (allowedOrigins.includes(origin)) {
+	  res.setHeader('Access-Control-Allow-Origin', origin);
+	}
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	res.setHeader('Access-Control-Allow-Credentials', true); 
+  
+	next();
+  });
+
+// Rutas
 app.use('/api/evento', Evento)
 
-app.listen(port, ()=>{
-	console.log(`escuchando en el puerto ${port}`)
-});
+// Manejo de rutas no encontradas
+app.use((req, res, next) => {
+	const error = new Error('Ruta no encontrada');
+	error.status = 404;
+	next(error);
+  });
+
+// Middleware para manejo de errores
+app.use((err, req, res, next) => {
+	res.status(err.status || 500);
+	res.json({
+	  error: {
+		message: err.message
+	  }
+	});
+  });
+	
+// Iniciar el servidor
+app.listen(PORT, () => {
+	console.log(`Servidor en funcionamiento en el puerto ${PORT}`);
+  });
+  
